a341e47f2aaa258b3a4ce465c32180b683889373
Quincy Le <lexuqing8@gmail.com>
2020-10-25 00:40:32 -0400

    feat(facets): Added facets support

--- e-shop-ui/src/App.js
+++ e-shop-ui/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
 import { List } from 'components/List'
+import { Facets } from 'components/Facets'
 import data from 'local-data'
 import {DataProcessor} from 'data/processor'
 // import logo from './logo.svg';
 import './App.css'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core'
+import GitHubIcon from '@material-ui/icons/GitHub'
 
+const FILTER_ENUM_KEYS = ['manufacturer', 'condition', 'productAspectRatio']
 const dataProcessor = new DataProcessor()
 const localData = dataProcessor.AddRating(data)
 
@@ -13,7 +19,23 @@ class App extends React.Component {
     isOnlineAvailable: false,
     isStoreAvailable: false,
     sortValue: 1,
-    data: localData
+    data: localData,
+    productFilters: localData.products.reduce((accumulator, current) => {
+      FILTER_ENUM_KEYS.forEach(filterKey => {
+        const filterValue = current[filterKey]
+
+        if (filterValue) {
+          if (!accumulator[filterKey]) accumulator[filterKey] = {}
+          if (!accumulator[filterKey][filterValue]) accumulator[filterKey][filterValue] = {
+            enabled: false,
+            count: 0
+          }
+
+          accumulator[filterKey][filterValue].count += 1
+        }
+      })
+      return accumulator
+    }, {})
   }
 
   changeOnlineAvailability = (event) => {
@@ -34,16 +56,61 @@ class App extends React.Component {
     })
   }
 
+  changeFacets = (filterKey, event) => {
+    this.setState({
+      productFilters: {
+        ...this.state.productFilters,
+        [filterKey]: {
+          ...this.state.productFilters[filterKey],
+          [event.target.name]: {
+            ...this.state.productFilters[filterKey][event.target.name],
+            enabled: event.target.checked
+          }
+        }
+      }
+    })
+  }
+
   processData = (data) => {
+    console.log('process data called.')
     const clonedData = {
       ...data,
       products: [...data.products] || []
     }
 
+    clonedData.products = this.filterByFacets(clonedData.products)
     clonedData.products = this.filterByAvailability(clonedData.products)
     clonedData.products = this.sortByPrice(clonedData.products)
 
     return clonedData
+  }
+
+  filterByFacets (items) {
+    const hasFacets = Object.entries(this.state.productFilters).some(([filterKey, filterValueHash]) => {
+      return Object.entries(filterValueHash).some(([filterValue, filterValueEntry]) => {
+        return filterValueEntry.enabled
+      })
+    })
+
+    if (!hasFacets) {
+      return items
+    }
+
+    return items.filter(item => {
+      return Object.entries(this.state.productFilters).every(([filterKey, filterValueHash]) => {
+        const filterValueSelected = Object.entries(filterValueHash).some(([filterValue, filterValueEntry]) => {
+          return filterValueEntry.enabled
+        })
+
+        if (!filterValueSelected) {
+          return true
+        }
+
+        return Object.entries(filterValueHash).some(([filterValue, filterValueEntry]) => {
+          return filterValueEntry.enabled && item[filterKey] === filterValue
+        })
+      })
+    })
   }
 
   filterByAvailability (items) {
@@ -83,27 +150,55 @@ class App extends React.Component {
     }
   }
 
+  clickGithubIcon = () => {
+    // window.location.href = "https://github.com/quincyle/e-shop/tree/master/e-shop-ui"
+  }
+
   render () {
     return (
-      <div 
-        style={{
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: '1280px',
+        margin: '0 auto',
+        position: 'relative',
+      }}>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" >
+                Cocoatale
+              </Typography>
+              {/* <Button color="inherit">Login</Button> */}
+              <IconButton edge="end" color="inherit" aria-label="menu"
+                onClick={this.clickGithubIcon}
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <div style={{margin: '20px 0', padding: '20px 0', fontSize: '22px', fontWeight: '400'}}>Gaming Monitors</div>
+        <div style={{
           display: 'flex',
-          maxWidth: '1280px',
-          margin: '0 auto',
-          position: 'relative',
-        }}
-      >
-        {/* <div className='facet-container'>facet</div> */}
-        <List 
-          data={this.processData(this.state.data)}
-          isOnlineAvailable={this.props.isOnlineAvailable}
-          isStoreAvailable={this.props.isStoreAvailable}
-          sortValue={this.state.sortValue}
+        }}>
+          <Facets 
+            filters={this.state.productFilters}
+            data={this.processData(this.state.data)}
 
-          changeOnlineAvailability={this.changeOnlineAvailability}
-          changeStoreAvailability={this.changeStoreAvailability}
-          changeSort={this.changeSort}
-        />
+            onFacetsChange={this.changeFacets}
+          />
+          <List 
+            data={this.processData(this.state.data)}
+            isOnlineAvailable={this.props.isOnlineAvailable}
+            isStoreAvailable={this.props.isStoreAvailable}
+            sortValue={this.state.sortValue}
+
+            changeOnlineAvailability={this.changeOnlineAvailability}
+            changeStoreAvailability={this.changeStoreAvailability}
+            changeSort={this.changeSort}
+          />
+        </div>
       </div>
     )
   }
---
+++ e-shop-ui/src/components/Facets.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+
+import 'css/Facets.css'
+
+
+const FILTER_KEYS = ['manufacturer', 'condition', 'productAspectRatio']
+
+export class Facets extends React.Component {
+  // filters
+  // data
+  // onFacetsChange
+
+  getFilterableValuesByKeys = (lists) => {
+    if (!lists.length) {
+      return
+    }
+
+    const filterableValuesByKeys = {}
+
+    FILTER_KEYS.forEach(filterKey => {
+      filterableValuesByKeys[filterKey] = [...new Set(lists.map(item => item[filterKey]).filter(i => i))]
+    })
+
+    return filterableValuesByKeys
+  } 
+  
+  render () {
+    return (
+      <div className='facet-container'>
+        {/* <Accordion> */}
+          {Object.entries(this.props.filters).map(([key, values]) => {
+            return (
+              <div key={key}>
+                <Accordion
+                  defaultExpanded={true}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    id="panel1a-header"
+                  >
+                    <Typography className="facet-key">{key.toUpperCase()}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                      {Object.entries(values).map(([filterValue, filterValueEntry]) => {
+                        return (
+                          <FormControlLabel
+                            className="facet-value-item"
+                            key={filterValue}
+                            control={
+                              <Checkbox
+                                checked={filterValueEntry.enabled}
+                                onChange={(event) => this.props.onFacetsChange(key, event)}
+                                name={filterValue}
+                                color="primary"
+                              />
+                            }
+                            label={`${filterValue} (${filterValueEntry.count})`}
+                          />
+                        )
+                      })}
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            )
+          })}
+      </div>
+    )
+  }
+}
